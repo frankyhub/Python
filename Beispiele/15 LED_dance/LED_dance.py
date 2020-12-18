e7144dc20f0c8338424b643869230b04b9d019ca
frankyhub <61152841+frankyhub@users.noreply.github.com>
2020-12-18 16:04:37 +0100

    Update LED_dance.py

--- Beispiele/15 LED_dance/LED_dance.py
+++ Beispiele/15 LED_dance/LED_dance.py
@@ -1,19 +1,12 @@
-# Light LEDs at random and make them fade over time
-#
-# Usage:
-#
-#    led_dance(delay)
-#
-# 'delay' is the time between each new LED being turned on.
-#
-# TODO The random number generator is not great. Perhaps the accelerometer
-# or compass could be used to add entropy.
+"""
+LED dance
+========= mu =========="""
 
 import microbit
 import random
 
 def led_dance(delay):
-    dots = [ [0]*5, [0]*5, [0]*5, [0]*5, [0]*5 ]
+    dots = [[0]*5, [0]*5, [0]*5, [0]*5, [0]*5]
     while True:
         dots[random.randrange(5)][random.randrange(5)] = 8
         for i in range(5):
